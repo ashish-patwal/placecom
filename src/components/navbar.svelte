674fe441fbef1c33f0e4133eacea29eb1129c587
ashish-patwal <ashishpatwal147@gmail.com>
2023-03-01 22:34:03 +0530

    first useful commit

---
+++ src/components/navbar.svelte
@@ -0,0 +1,25 @@
+<nav class="p-3 absolute left-0 top-0 w-full text-white md:flex md:items-center md:justify-between">
+	<div>
+		<span class="text-4xl font-bold font-navbar cursor-pointer">Placecom</span>
+	</div>
+	<ul class="md:flex md:items-center md:static absolute w-full left-0 md:w-auto">
+		<li class="mx-4 my-4 md:my-0">
+			<button
+				class="bg-gray-400/30 duration-300 hover:bg-green-500 text-lg font-navbar p-2 rounded-md text-white"
+				>Contact</button
+			>
+		</li>
+		<li class="mx-4 my-4 md:my-0">
+			<button
+				class="bg-gray-400/30 duration-300 hover:bg-green-500 text-lg font-navbar p-2 rounded-md text-white"
+				>Discord</button
+			>
+		</li>
+		<li class="mx-4 my-4 md:my-0">
+			<button
+				class="bg-gray-400/30 duration-300 hover:bg-green-500 text-lg font-navbar p-2 rounded-md text-white"
+				>About</button
+			>
+		</li>
+	</ul>
+</nav>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				navbar: ['Open Sans']
+			}
+		}
+	},
+	plugins: []
+}

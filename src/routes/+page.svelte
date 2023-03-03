23af83e5f8a3e9ca3ab3bda84a5a6570b6ef52dc
ashish-patwal <ashishpatwal147@gmail.com>
2023-03-04 00:44:09 +0530

    duhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,29 +32,39 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section class="relative">
-	{#if ready}
-		<div
-			class="flex border-white p-4 border-8 flex-col absolute leading-[4rem] sm:text-center font-extrabold font-sans text-white left-0 right-0 sm:mx-auto  md:text-8xl w-min sm:text-7xl text-6xl top-24 mx-3.5"
-			transition:fly={{ y: 200, duration: 1300 }}
-		>
-			{#each heading as word, index}
-				<h1
-					class="md:self-center max-w-min"
-					data-value={word}
-					on:focus
-					on:mouseover={handleHackerText}
-					transition:fly={{ y: 100, duration: 1500 + index * 150 }}
-				>
-					{word}
-				</h1>
-			{/each}
-		</div>
-	{/if}
-	<picture>
-		<source srcset={homecover} type="image/jpg" />
-		<img class="h-[100rem] max-w-none" srcset={homecover} alt="homecover" />
-	</picture>
-
-	<h1 class="text-white">Dumb Dumb</h1>
-</section>
+<main>
+	<section class="relative">
+		{#if ready}
+			<div
+				class="flex border-white p-4 border-8 flex-col absolute leading-[4rem] sm:text-center font-extrabold font-sans text-white left-0 right-0 sm:mx-auto  md:text-8xl w-min sm:text-7xl text-6xl top-24 mx-3.5"
+				transition:fly={{ y: 200, duration: 1300 }}
+			>
+				{#each heading as word, index}
+					<h1
+						class="md:self-center max-w-min"
+						data-value={word}
+						on:focus
+						on:mouseover={handleHackerText}
+						transition:fly={{ y: 100, duration: 1500 + index * 150 }}
+					>
+						{word}
+					</h1>
+				{/each}
+			</div>
+		{/if}
+		<picture>
+			<source srcset={homecover} type="image/jpg" />
+			<img class="h-[100rem] max-w-none" srcset={homecover} alt="homecover" />
+		</picture>
+	</section>
+	<section class="w-full h-screen" />
+	<section
+		class="w-full h-screen relative after:bg-gradient-to-br after:from-layerone-pink-three after:via-layerone-pink-two after:to-layerone-pink-one after:content-[''] after:inset-0 after:absolute after:skew-y-6"
+	/>
+	<section
+		class="w-full h-screen relative after:bg-gradient-to-br after:from-layertwo-neon-three after:via-layertwo-neon-two after:to-layertwo-neon-one after:content-[''] after:inset-0 after:absolute after:skew-y-6"
+	/>
+	<section
+		class="w-full h-screen relative after:bg-gradient-to-br after:from-layerthree-violet-three after:via-layerthree-violet-two after:to-layerthree-violet-one after:content-[''] after:inset-0 after:absolute after:skew-y-6"
+	/>
+</main>

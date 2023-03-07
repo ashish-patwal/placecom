75880cc4f5ce18eac692557936883930fe0ba04b
ashish-patwal <ashishpatwal147@gmail.com>
2023-03-08 00:41:36 +0530

    dumb

---
+++ src/components/gradient-card.svelte
@@ -0,0 +1,133 @@
+<script>
+	import { fly } from 'svelte/transition'
+	let kardhovering = false
+	const info =
+		'As we know that in India, placement drives are generally conducted for technical courses like B tech, MBA, polytechnic, etc; but in this race of placement, students with non technical background are somewhere left behind and here comes our PlaceCom, which is a Roorkee based startup that focuses on conducting placement drives for general courses like B.Sc, B.Com, B.Ed, B pharma and many more so that the students of these courses get more job opportunities and in-hand experience of campus placement.'
+</script>
+
+<div class="kard">
+  <div
+    class="kard-content"
+    on:blur
+    on:focus
+    on:mouseover={() => {
+      kardhovering = true
+    }}
+    on:mouseout={() => {
+      kardhovering = false
+    }}
+    >
+    <h3 class="kard-title font-aneklatin">Who are we exactly ?</h3>
+    {#if kardhovering}
+      <h5 class="kard-subtitle font-aneklatin" transition:fly={{ y: 100, duration: 1000 }}>
+        {info}
+      </h5>
+    {/if}
+  </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="kard-icon md:w-13 h-9 w-9 text-white md:w-12"
+    >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+    />
+  </svg>
+</div>
+
+<style>
+	:root {
+		--border: rgb(3, 169, 244);
+
+		--g1: rgb(98, 0, 234);
+		--g2: rgb(236, 64, 122);
+		--g3: rgb(253, 216, 53);
+	}
+	.kard {
+		aspect-ratio: 1 / 1.4;
+		border: 0.5vmin solid var(--border);
+		cursor: pointer;
+		position: relative;
+		width: 60vmin;
+		display: flex;
+		align-self: center;
+	}
+
+	.kard:hover:before {
+		background-position: 100% 100%;
+		transform: scale(1.08, 1.03);
+	}
+
+	.kard:hover > .kard-content {
+		background-position: -10% 0%;
+	}
+
+	.kard:hover > .kard-icon {
+		color: white;
+	}
+
+	.kard:before {
+		background: linear-gradient(
+			130deg,
+			transparent 0% 33%,
+			var(--g1) 66%,
+			var(--g2) 83.5%,
+			var(--g3) 100%
+		);
+		background-position: 0% 0%;
+		background-size: 300% 300%;
+		content: '';
+		height: 100%;
+		left: 0px;
+		pointer-events: none;
+		position: absolute;
+		top: 0px;
+		transition: background-position 350ms ease, transform 350ms ease;
+		width: 100%;
+		z-index: 1;
+	}
+
+	.kard-content {
+		background-image: radial-gradient(rgba(255, 255, 255, 0.2) 8%, transparent 8%);
+		background-position: 0% 0%;
+		background-size: 5vmin 5vmin;
+		height: 100%;
+		padding: 5vmin;
+		position: relative;
+		transition: background-position 350ms ease;
+		width: 100%;
+		z-index: 2;
+	}
+
+	.kard-title,
+	.kard-subtitle {
+		color: white;
+		font-weight: 400;
+		margin: 0px;
+	}
+
+	.kard-title {
+		font-size: 5vmin;
+	}
+
+	.kard-subtitle {
+		font-size: 2.5vmin;
+		margin-top: 2vmin;
+	}
+
+	.kard-icon {
+		bottom: 0px;
+		color: rgba(255, 255, 255, 0.5);
+		font-size: 7vmin;
+		right: 0px;
+		margin: 5vmin;
+		position: absolute;
+		transition: color 250ms ease;
+		z-index: 2;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte'
 	import { fly } from 'svelte/transition'
-	import tilt from '../components/tilt.js'
+  import GradientCard from '../components/gradient-card.svelte'
 	import homecover from '$lib/images/homecover.jpg'
 	import student from '$lib/images/student.svg'
 	import interview from '$lib/images/interview.svg'
@@ -35,9 +35,6 @@
 		}, 30)
 	}
 
-	let kardhovering = false
-	const info =
-		'As we know that in India, placement drives are generally conducted for technical courses like B tech, MBA, polytechnic, etc; but in this race of placement, students with non technical background are somewhere left behind and here comes our PlaceCom, which is a Roorkee based startup that focuses on conducting placement drives for general courses like B.Sc, B.Com, B.Ed, B pharma and many more so that the students of these courses get more job opportunities and in-hand experience of campus placement.'
 </script>
 
 <svelte:head>
@@ -84,79 +81,19 @@
 			<div class="carousel-center carousel rounded-box w-72 space-x-4 md:w-80 lg:w-96">
 				<div id="slide1" class="carousel-item relative w-full">
 					<img src={student} class="w-full" alt="Tailwind CSS Carousel component" />
-					<div
-						class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
-					>
-						<a href="#slide3" class="btn-warning btn-circle btn">❮</a>
-						<a href="#slide2" class="btn-warning btn-circle btn">❯</a>
-					</div>
 				</div>
 				<div id="slide2" class="carousel-item relative w-full">
 					<img src={interview} class="w-full" alt="Tailwind CSS Carousel component" />
-					<div
-						class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
-					>
-						<a href="#slide1" class="btn-warning btn-circle btn">❮</a>
-						<a href="#slide3" class="btn-warning btn-circle btn">❯</a>
-					</div>
 				</div>
 				<div id="slide3" class="carousel-item relative w-full">
 					<img src={career} class="w-full" alt="Tailwind CSS Carousel component" />
-					<div
-						class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
-					>
-						<a href="#slide2" class="btn-warning btn-circle btn">❮</a>
-						<a href="#slide1" class="btn-warning btn-circle btn">❯</a>
-					</div>
 				</div>
 			</div>
 		</div>
-		<!-- <div class="card w-96 h-max bg-base-100 shadow-xl"> -->
-		<!-- 	<div class="card-body"> -->
-		<!-- 		<h2 class="card-title">Card title!</h2> -->
-		<!-- 		<p>If a dog chews shoes whose shoes does he choose?</p> -->
-		<!-- 		<div class="card-actions justify-end"> -->
-		<!-- 			<button class="btn btn-primary">Buy Now</button> -->
-		<!-- 		</div> -->
-		<!-- 	</div> -->
-		<!-- </div> -->
-		<div class="kard">
-			<div
-				class="kard-content"
-				on:blur
-				on:focus
-				on:mouseover={() => {
-					kardhovering = true
-				}}
-				on:mouseout={() => {
-					kardhovering = false
-				}}
-			>
-				<h3 class="kard-title font-aneklatin">Who are we exactly ?</h3>
-				{#if kardhovering}
-					<h5 class="kard-subtitle font-aneklatin" transition:fly={{ y: 100, duration: 1000 }}>
-						{info}
-					</h5>
-				{/if}
-			</div>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="kard-icon md:w-13 h-9 w-9 text-white md:w-12"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
-				/>
-			</svg>
-		</div>
+    <GradientCard/>
 	</section>
 	<section
-		class="relative flex h-full w-full flex-wrap bg-gradient-to-br from-layerone-pink-three via-layerone-pink-two to-layerone-pink-one p-10 md:h-screen "
+		class="relative flex justify-center h-full w-full flex-wrap bg-gradient-to-br from-layerone-pink-three via-layerone-pink-two to-layerone-pink-one p-10 md:h-screen "
 	>
 		<div class="flex basis-1/2 flex-col justify-center gap-6 font-poppins text-lime">
 			<h1 class="text-5xl font-bold">PROPOSAL PRESENTATION</h1>
@@ -174,20 +111,17 @@
 				Tempora, quam.
 			</p>
 		</div>
-		<div class="flex basis-1/2 justify-center">
+		<div  class="flex basis-1/2 justify-center">
 			<img
-				use:tilt={{ scale: 1.1, max: 5 }}
+        class="hover:scale-110 hover:brightness-105 transition-all delay-100 ease-in-out"
 				src={proposalPresentation}
 				alt="proposal presentation"
 			/>
 		</div>
 	</section>
 	<section
-		class="relative flex h-full w-full flex-wrap bg-gradient-to-br from-layertwo-neon-three via-layertwo-neon-two to-layertwo-neon-one p-10 md:h-screen"
+		class="relative flex justify-center h-full w-full flex-wrap bg-gradient-to-br from-layertwo-neon-three via-layertwo-neon-two to-layertwo-neon-one p-10 md:h-screen"
 	>
-		<div class="flex basis-1/2 justify-center">
-			<img use:tilt={{ scale: 1.1, max: 5 }} src={placementPlanning} alt="placement planning" />
-		</div>
 		<div class="flex basis-1/2 flex-col justify-center gap-6 font-poppins">
 			<h1 class="text-5xl font-bold">PLACEMENT PLANNING</h1>
 			<p>
@@ -203,6 +137,11 @@
 				fugit corrupti quidem dolorum, perspiciatis nostrum esse recusandae deserunt adipisci.
 				Tempora, quam.
 			</p>
+		</div>
+		<div class="flex md:-order-1 basis-1/2 justify-center">
+			<img 
+        class="hover:scale-110 hover:brightness-105 transition-all delay-100 ease-in-out"
+        src={placementPlanning} alt="placement planning" />
 		</div>
 	</section>
 	<section
@@ -224,106 +163,22 @@
 				Tempora, quam.
 			</p>
 		</div>
-		<div class="flex basis-1/4 flex-col justify-end">
-			<img src={student2} alt="student 2" />
-		</div>
-		<div class="flex basis-1/5 flex-col justify-end">
-			<img src={student3} alt="student 3" />
-		</div>
-		<div class="flex basis-1/6 flex-col justify-end">
-			<img src={student4} alt="student 4" />
-		</div>
+    <div class="flex md:flex-row flex-col basis-1/2 justify-center">
+      <div class="flex basis-1/3 flex-col justify-center">
+        <img 
+          class="hover:scale-110 hover:brightness-105 transition-all delay-100 ease-in-out"
+          src={student2} alt="student 2" />
+      </div>
+      <div class="flex basis-1/3 flex-col justify-center">
+        <img 
+          class="hover:scale-110 hover:brightness-105 transition-all delay-100 ease-in-out"
+          src={student3} alt="student 3" />
+      </div>
+      <div class="flex basis-1/4 flex-col justify-center">
+        <img 
+          class="hover:scale-110 hover:brightness-105 transition-all delay-100 ease-in-out"
+          src={student4} alt="student 4" />
+      </div>
+    </div>
 	</section>
 </main>
-
-<style>
-	:root {
-		--border: rgb(3, 169, 244);
-
-		--g1: rgb(98, 0, 234);
-		--g2: rgb(236, 64, 122);
-		--g3: rgb(253, 216, 53);
-	}
-	.kard {
-		aspect-ratio: 1 / 1.4;
-		border: 0.5vmin solid var(--border);
-		cursor: pointer;
-		position: relative;
-		width: 60vmin;
-		display: flex;
-		align-self: center;
-	}
-
-	.kard:hover:before {
-		background-position: 100% 100%;
-		transform: scale(1.08, 1.03);
-	}
-
-	.kard:hover > .kard-content {
-		background-position: -10% 0%;
-	}
-
-	.kard:hover > .kard-icon {
-		color: white;
-	}
-
-	.kard:before {
-		background: linear-gradient(
-			130deg,
-			transparent 0% 33%,
-			var(--g1) 66%,
-			var(--g2) 83.5%,
-			var(--g3) 100%
-		);
-		background-position: 0% 0%;
-		background-size: 300% 300%;
-		content: '';
-		height: 100%;
-		left: 0px;
-		pointer-events: none;
-		position: absolute;
-		top: 0px;
-		transition: background-position 350ms ease, transform 350ms ease;
-		width: 100%;
-		z-index: 1;
-	}
-
-	.kard-content {
-		background-image: radial-gradient(rgba(255, 255, 255, 0.2) 8%, transparent 8%);
-		background-position: 0% 0%;
-		background-size: 5vmin 5vmin;
-		height: 100%;
-		padding: 5vmin;
-		position: relative;
-		transition: background-position 350ms ease;
-		width: 100%;
-		z-index: 2;
-	}
-
-	.kard-title,
-	.kard-subtitle {
-		color: white;
-		font-weight: 400;
-		margin: 0px;
-	}
-
-	.kard-title {
-		font-size: 5vmin;
-	}
-
-	.kard-subtitle {
-		font-size: 2.5vmin;
-		margin-top: 2vmin;
-	}
-
-	.kard-icon {
-		bottom: 0px;
-		color: rgba(255, 255, 255, 0.5);
-		font-size: 7vmin;
-		right: 0px;
-		margin: 5vmin;
-		position: absolute;
-		transition: color 250ms ease;
-		z-index: 2;
-	}
-</style>
